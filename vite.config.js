cb8133f15dc446758f3dd78ed9e37142fed59dce
Jevgenija Potalujeva <jpotalujeva@gmail.com>
2024-07-25 19:10:34 +0300

    revert

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    host: '0.0.0.0',
+    host: 'localhost',
         hmr: {
-            host: 'localhost'
+            host: 'localhost',
         },
     plugins: [
         laravel({
